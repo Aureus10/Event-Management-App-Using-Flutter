16d62a14bc6d401780c71bcd7c14b8c45c3cbc02
Aureus10 <141618112+Aureus10@users.noreply.github.com>
2024-06-30 08:48:36 +0800

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:assignment/widgets/headerBar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: HeaderBar(title: "Hello world")
+      ),
+    );
+  }
+}
---
+++ lib/widgets/headerBar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HeaderBar extends StatelessWidget {
+  const HeaderBar({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title, 
+          style: const TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xff27a1ff),
+        shadowColor: Colors.black,
+        elevation: 30,
+      ),
+    );
+  }
+
+
+}
