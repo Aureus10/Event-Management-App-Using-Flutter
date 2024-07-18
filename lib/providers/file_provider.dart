7c67f425fa8e81323bee155da11d774e0cbc84ed
Aureus10 <141618112+Aureus10@users.noreply.github.com>
2024-07-18 11:34:23 +0800

    revise file repo and provider

--- lib/providers/file_provider.dart
+++ lib/providers/file_provider.dart
@@ -8,10 +8,10 @@ class FileProvider extends ChangeNotifier {
   static final FileRepository _fileRepository = FileRepository();
 
   Future<String?> uploadProfileImage(File image, String email) async {
-    return await _fileRepository.uploadImage(image, 'images/profile/$email');
+    return await _fileRepository.uploadFile(image, 'images/profile/$email');
   }
 
   static Future<String?> uploadEventImage(File image, String id) async {
-    return await _fileRepository.uploadImage(image, 'images/event/$id');
+    return await _fileRepository.uploadFile(image, 'images/event/$id');
   }
 }
